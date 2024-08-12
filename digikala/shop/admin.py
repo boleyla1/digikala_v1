4497c30cb87a7dd17c90da7e0234c69954dd8ab3
Awm1r Official <amirm4a1@gmail.com>
2024-08-12 21:06:25 +0330

    Push For Mehrshad

--- digikala/shop/admin.py
+++ digikala/shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from shop.models import *
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(SliderItem)
+admin.site.register(News)
--- digikala/shop/views.py
+++ digikala/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    Slider_Item = SliderItem.objects.all()
+    return render(request, 'main/index.html', {'Slider_Item': Slider_Item })
